294c682b9bbc302a670dc90c1936dfdfbcd2e929
natac <natac@DESKTOP-JK8TO4T>
2026-04-09 09:16:32 +0300

    updates forms
    Добавлены элементы управления и изменен интерфейс формы
    
    В файле MyForm.h добавлены метка (Label), флажок (CheckBox) и кнопка (Button) с изменением их свойств, таких как расположение и размер. Также изменен размер формы и цвет фона, а новые элементы управления добавлены в коллекцию Controls.
    
    В файле MyForm.resx удалены метаданные, связанные с элементом управления folderBrowserDialog1.

--- test_proga_forms/MyForm.h
+++ test_proga_forms/MyForm.h
@@ -35,6 +35,8 @@ namespace testprogaforms {
 			}
 		}
 	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::CheckBox^ checkBox1;
 	protected:
 
 	private:
@@ -51,27 +53,52 @@ namespace testprogaforms {
 		void InitializeComponent(void)
 		{
 			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(490, 210);
+			this->button1->Location = System::Drawing::Point(91, 104);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(456, 90);
+			this->button1->Size = System::Drawing::Size(368, 88);
 			this->button1->TabIndex = 0;
 			this->button1->Text = L"button1";
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
 			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(87, 52);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(51, 20);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"label1";
+			// 
+			// checkBox1
+			// 
+			this->checkBox1->AutoSize = true;
+			this->checkBox1->Location = System::Drawing::Point(91, 235);
+			this->checkBox1->Name = L"checkBox1";
+			this->checkBox1->Size = System::Drawing::Size(113, 24);
+			this->checkBox1->TabIndex = 2;
+			this->checkBox1->Text = L"checkBox1";
+			this->checkBox1->UseVisualStyleBackColor = true;
+			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1456, 529);
+			this->BackColor = System::Drawing::SystemColors::ActiveCaption;
+			this->ClientSize = System::Drawing::Size(1456, 603);
+			this->Controls->Add(this->checkBox1);
+			this->Controls->Add(this->label1);
 			this->Controls->Add(this->button1);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
